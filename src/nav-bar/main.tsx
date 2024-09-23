7318bc95e515549c0b3b02b2692ecf18b514ca44
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2024-09-23 17:54:11 -0500

    Added NavBar

---
+++ src/nav-bar/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './styles/nav-bar.module.css';
+import ieeeLogo from "../assets/ieeelogotransparent.png";
+
+function NavBar() {
+    return <header>
+    <div className={styles.container}>
+      <img className={styles.logo} src={ieeeLogo} alt="Logo" />
+
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Tutoring</a></li>
+          <li><a href="#">Officers</a></li>
+          <li><a href="#">Society</a></li>
+          <li><a href="#">Join</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>;
+}
+
+export default NavBar;
