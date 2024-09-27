81806fcd7760cf7d7113651059c24a66c89b0e83
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2024-09-26 19:52:30 -0500

    Finished Navbar + Responsive

--- src/nav-bar/nav-bar.tsx
+++ src/nav-bar/nav-bar.tsx
@@ -1,40 +1,136 @@
 import styles from "./nav-bar.module.css";
 import ieeeLogo from "../assets/ieeelogotransparent.png";
+import accountIcon from "../assets/account-white-icon.png";
+function MobileDetect() {
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw(n|u)|c55\/|capi|ccwa|cdm|cell|chtm|cldc|cmd|co(mp|nd)|craw|da(it|ll|ng)|dbte|dcs|devi|dica|dmob|do(c|p)o|ds(12|d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(|_)|g1 u|g560|gene|gf5|gmo|go(\.w|od)|gr(ad|un)|haie|hcit|hd(m|p|t)|hei|hi(pt|ta)|hp( i|ip)|hsc|ht(c(| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i(20|go|ma)|i230|iac( ||\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|[a-w])|libw|lynx|m1w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|mcr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|([1-8]|c))|phil|pire|pl(ay|uc)|pn2|po(ck|rt|se)|prox|psio|ptg|qaa|qc(07|12|21|32|60|[2-7]|i)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h|oo|p)|sdk\/|se(c(|0|1)|47|mc|nd|ri)|sgh|shar|sie(|m)|sk0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h|v|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl|tdg|tel(i|m)|tim|tmo|to(pl|sh)|ts(70|m|m3|m5)|tx9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas|your|zeto|zte/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true;
+  })(navigator.userAgent);
+  return check;
+}
 
 function NavBar() {
-  //If Signed In Remove replace sign in button with profile icon
-
   const signedIn = false;
+
   return (
     <header>
-      <div className={styles.container}>
-        <img className={styles.logo} src={ieeeLogo} alt="Logo" />
+      <div className={styles.mobileContainer}>
+        <nav className={styles.mobileNav} role="navigation">
+          <div id={styles.menuToggle}>
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id={styles.menu}>
+              <li>
+                <a className={styles.mobileLink} href="/">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className={styles.mobileLink} href="/events">
+                  Events
+                </a>
+              </li>
+              <li>
+                <a className={styles.mobileLink} href="/tutoring">
+                  Tutoring
+                </a>
+              </li>
+              <li>
+                <a className={styles.mobileLink} href="/officers">
+                  Officers
+                </a>
+              </li>
+              <li>
+                <a className={styles.mobileLink} href="/society">
+                  Society
+                </a>
+              </li>
+              <li>
+                <a className={styles.mobileLink} href="/join">
+                  Join
+                </a>
+              </li>
+              <li>
+                {signedIn ? (
+                  <a href="/account" className={styles.mobileLink}>
+                    Account
+                  </a>
+                ) : (
+                  <a href="/signin" className={styles.mobileLink}>
+                    Sign In
+                  </a>
+                )}
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className={styles.mobileLogo}>
+          <a href="/" className={styles.mobileLogoText}>
+            IEEE UTD
+          </a>
+          <img className={styles.mobileLogoImg} src={ieeeLogo} alt="IEEE Logo" />
+        </div>
+      </div>
+      <div className={styles.desktopContainer}>
+        <a href="/">
+          <img className={styles.logo} src={ieeeLogo} alt="IEEE Logo" />
+        </a>
 
-        <nav>
+        <nav className={styles.navDesktop}>
           <ul>
             <li>
-              <a href="/">Home</a>
+              <a className={styles.link} href="/">
+                Home
+              </a>
             </li>
             <li>
-              <a href="/Events">Events</a>
+              <a className={styles.link} href="/events">
+                Events
+              </a>
             </li>
             <li>
-              <a href="/Tutoring">Tutoring</a>
+              <a className={styles.link} href="/tutoring">
+                Tutoring
+              </a>
             </li>
             <li>
-              <a href="/Officers">Officers</a>
+              <a className={styles.link} href="/officers">
+                Officers
+              </a>
             </li>
             <li>
-              <a href="/Society">Society</a>
+              <a className={styles.link} href="/society">
+                Society
+              </a>
             </li>
             <li>
-              <a href="/Join">Join</a>
+              <a className={styles.link} href="/join">
+                Join
+              </a>
             </li>
             <li>
               {signedIn ? (
-                <button className={styles["sign-in-btn"]}>Account</button>
+                <a href="/account">
+                  <img
+                    className={`${styles.accountIcon} ${styles.link}`}
+                    src={accountIcon}
+                    alt="Account Icon"
+                  />
+                </a>
               ) : (
-                <button className={styles["sign-in-btn"]}>Sign In</button>
+                <a href="/signin">
+                  <button className={styles["sign-in-btn"]}>Sign In</button>
+                </a>
               )}
             </li>
           </ul>
