bd44eb0f74cf22c24868d276fc330aa255387919
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2024-10-03 19:26:00 -0500

    Added Sponsors

--- src/home-page/home-page.tsx
+++ src/home-page/home-page.tsx
@@ -1,12 +1,16 @@
 //Use cookies for log in
 
+import Sponsors from "./components/sponsors/sponsors";
+
+import "./home-page.css";
+
 //https://ieeeutd.org/privacy-policy
 
 function HomePage() {
   return (
-    <>
-
-    </>
+    <div className="home-page">
+      <Sponsors />
+    </div>
   );
 }
 
