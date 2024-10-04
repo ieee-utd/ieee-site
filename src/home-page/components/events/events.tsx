5ecaa25c5d66b80eadef4496b1ab3193f1b7d334
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2024-10-03 19:25:26 -0500

    Added Sponsors

---
+++ src/home-page/components/events/events.tsx
@@ -0,0 +1,9 @@
+function Events() {
+    //generate events based on list in db
+    //events:
+    //event title, image, description, link
+    //display events by order of closest first
+    //carousel
+}
+
+export default Events;
---
+++ src/home-page/components/sponsors/sponsors.tsx
@@ -0,0 +1,8 @@
+import styles from "./sponsors.module.css";
+
+function Sponsors() {
+
+    return <div className={styles.container}></div>;
+}
+
+export default Sponsors;
