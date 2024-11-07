5e33073f2781e233f0704f3bd42326a84c181716
Kushals-Air <kushalchoudhary3121@gmail.com>
2024-11-06 18:06:26 -0600

    Added the chatbot window

--- src/home-page/home-page.tsx
+++ src/home-page/home-page.tsx
@@ -1,10 +1,11 @@
 //Use cookies for log in
 
 //https://ieeeutd.org/privacy-policy
-import TutoringCalendar from './tutoring-calendar/tutoring-calendar';
-import WhoWeAre from '../who-we-are/who-we-are';
+import TutoringCalendar from "./tutoring-calendar/tutoring-calendar";
+import WhoWeAre from "../who-we-are/who-we-are";
 import Sponsors from "./sponsors/sponsors";
 import Landing from "./landing/landing";
+import Chatbot from "../chatbot/components/chatbot";
 
 function HomePage() {
   return (
@@ -13,6 +14,7 @@ function HomePage() {
       <Sponsors />
       <WhoWeAre />
       <TutoringCalendar />
+      <Chatbot />
     </>
   );
 }
