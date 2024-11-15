04b36718c731adaa33c8bf6deaba8bf91faff653
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2024-11-14 21:34:33 -0600

    Added Sponsors

---
+++ src/officers/officerData.tsx
@@ -0,0 +1,122 @@
+const officerData = [
+    {
+        name: "Allison Pham",
+        title: "President",
+        image: require("../assets/IEEE/Allison.jpeg"),
+        linkedin: "https://www.linkedin.com/in/allison-nhi-pham/",
+        email: "anp200002@utdallas.edu",
+    },
+    {
+        name: "Jacob Martin",
+        title: "Vice President",
+        image: require('../assets/IEEE/Jacob.jpeg'),
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "jrm190007@utdallas.edu",
+    },
+    {
+        name: "Gaby Tobias",
+        title: "Treasurer",
+        image: require("../assets/IEEE/Gaby.jpeg"),
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "gst200000@utdallas.edu",
+    },
+    {
+        name: "Alex Monroy",
+        title: "Secretary",
+        image: require("../assets/IEEE/Alex.jpeg"), // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "aas170530@utdallas.edu",
+    },
+    {
+        name: "Kevin Debord",
+        title: "Advisor",
+        image: require("../assets/IEEE/Kevin.jpeg"), // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "kjd200004@utdallas.edu",
+    },
+    {
+        name: "Rushil Sivaiah",
+        title: "Secretary",
+        image: "",
+        linkedin: "rrs220003@utdallas.edu", // Please provide the LinkedIn profile if available
+    },
+    {
+        name: "Nethra",
+        title: "Director of Tutoring",
+        image: require("../assets/IEEE/Nethra.jpeg"), // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "nxs200035@utdallas.edu",
+    },
+    {
+        name: "Laura Pinto",
+        title: "Director of Operations",
+        image: require("../assets/IEEE/Laura.jpeg"), // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "lsp210000@utdallas.edu",
+    },
+    {
+        name: "James Odebiyi",
+        title: "Web Master",
+        image: require("../assets/IEEE/James.jpeg"),
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "jxo190003@utdallas.edu",
+    },
+    {
+        name: "Mason",
+        title: "Director of Business",
+        image: require("../assets/IEEE/mason.jpeg"), 
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "msr200000@utdallas.edu",
+    },
+    {
+        name: "James Dao",
+        title: "RF Director",
+        image: "", // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "jkd190003@utdallas.edu"
+    },
+    {
+        name: "Jane Bae",
+        title: "RF Director",
+        image: require("../assets/IEEE/Jane.jpeg"),
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "jxb190001@utdallas.edu"
+    },
+    {
+        name: "Muneeb",
+        title: "RF Project Supervisor",
+        image: require("../assets/IEEE/Muneeb.jpeg"), // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "mss200007@utdallas.edu",
+    },
+    {
+        name: "Zaid",
+        title: "PES Project Supervisor",
+        image: require("../assets/IEEE/Zaid.jpeg"), // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "mxz190024@utdallas.edu",
+    },
+    {
+        name: "Manuel Contreras",
+        title: "PES Director",
+        image: require("../assets/IEEE/Manuel.jpeg"), // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "mad210014@utdallas.edu",
+    },
+    {
+        name: "Ian Kintz",
+        title: "Director of Robotics",
+        image: require("../assets/IEEE/Ian.jpeg"), // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "ijk180001@utdallas.edu",
+    },
+    {
+        name: "Mandy Hardono",
+        title: "RAS Director",
+        image: "", // Please provide the image if available
+        linkedin: "", // Please provide the LinkedIn profile if available
+        email: "vah200000@utdallas.edu",
+    }
+];
+
+export default officerData;
