96082203e34b071320fea93c5acd29b03bbe98c0
knihal7 <nihaalbiz@gmail.com>
2024-11-16 23:59:47 -0600

    Initialized section

--- src/home-page/home-page.tsx
+++ src/home-page/home-page.tsx
@@ -6,6 +6,7 @@ import WhoWeAre from "../who-we-are/who-we-are";
 import Sponsors from "./sponsors/sponsors";
 import Landing from "./landing/landing";
 import Chatbot from "../chatbot/components/chatbot";
+import WhatWeDo from "../what-we-do/what-we-do";
 
 function HomePage() {
   return (
@@ -13,6 +14,7 @@ function HomePage() {
       <Landing />
       <Sponsors />
       <WhoWeAre />
+      <WhatWeDo />
       <TutoringCalendar />
       <Chatbot />
     </>
